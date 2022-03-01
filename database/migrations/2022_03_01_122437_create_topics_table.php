69d2b2aec0cb5a256b1ff76a0d599bdf671a7333
yugo uchiida <y.uchiida@nirneege.com>
2022-03-01 21:41:19 +0900

    create migration files

---
+++ database/migrations/2022_03_01_122437_create_topics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(); /* 外部キー制約つきのuser_idカラムを作成 */
+            $table->string('name');
+            $table->boolean('is_user_checked');
+            $table->timestamp('latest_comment_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+};
---
+++ database/migrations/2022_03_01_122514_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+            $table->foreignId('topics_id')->constrained(); /* 外部キー制約付きのtopics_idカラムを作成 */
+            $table->foreignId('user_id')->constrained(); /* 外部キー制約つきのuser_idカラムを作成 */
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
