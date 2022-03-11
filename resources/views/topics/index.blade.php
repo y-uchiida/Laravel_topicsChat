0be3676b81bbc07071f8f1f148e831fbe942824a
yugo uchiida <y.uchiida@nirneege.com>
2022-03-12 02:01:39 +0900

    improved: add message-form blade component, and replaced from existing form to component

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -38,25 +38,7 @@
                     @endforeach
                     <hr class="my-2 border-b border-gray-200">
                     <div class="new_message_form">
-                        <form method="POST" action="/topics/{{$topic->id}}/messages" class="mb-5">
-                            @csrf
-                            <div class="flex flex-col space-y-4 mt-2 mb-4">
-                                <div class="form-group">
-                                    <label for="topic-first-content"
-                                        class="block text-sm font-medium text-gray-900 dark:text-gray-500">内容</label>
-                                    <textarea name="body"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
-                                        id="topic-first-content" rows="3" required></textarea>
-                                </div>
-
-                                <div class="form-group flex flex-row-reverse">
-                                    <button type="submit"
-                                        class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
-                                        書き込む
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                        <x-message-form :topic="$topic" />
                     </div>
                     <div>
                         <a href="{{ route('topics.show', $topic->id) }}">全部読む</a>
