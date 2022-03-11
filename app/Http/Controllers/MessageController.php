11d1f60688733ac7b75db03ae7a9c1a33a126968
yugo uchiida <y.uchiida@nirneege.com>
2022-03-12 02:46:30 +0900

    implemented: feature to upload attached images to AWS S3 storage

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 use App\Http\Requests\MessageRequest;
 use App\Services\MessageService;
+use App\Models\Image;
 
 class MessageController extends Controller
 {
@@ -51,6 +53,21 @@ class MessageController extends Controller
             /* レコード作成に必要なデータ（カラムに入る内容）を配列にまとめる */
             $message_data = $request->validated();
             $message_data['user_id'] = Auth::id();
+
+            $message = $this->message_service->createNewMessage($message_data, $id);
+
+            $images = $request->file('images'); /* 送信された画像を$images に代入 */
+            //
+            if ($images) {
+                foreach($images as $image){
+                    $path = Storage::disk('s3')->put('/', $image); /* AWSのS3バケットにアップロードを実行 */
+                    /* DBにも画像の情報を保存 */
+                    $image = new Image();
+                    $image->file_path = $path;
+                    $image->message_id = $message->id;
+                    $image->save();
+                }
+            }
 
             /* Service 経由で、メッセージの保存を実行 */
             $this->message_service->createNewMessage($message_data, $id);
